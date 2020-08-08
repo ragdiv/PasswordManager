264ec4da572431fb1a2c249eb0bf734ca226e69b
Maremanda <RMaremanda@aamva.org>
2020-08-08 14:20:43 -0400

    Add project files.

---
+++ PasswordManager/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace PasswordManager.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            //ViewData["Message"] = "My key val = " + _configuration["mongocustomerprofile"];
+        }
+        private static IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
---
+++ PasswordManager/Pages/Login.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PasswordManager.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+        public void OnGet()
+        {
+          
+        }
+
+        public ActionResult OnPost()
+        {
+           
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("Login");
+
+        }
+    }
+}
